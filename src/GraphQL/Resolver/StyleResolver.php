4332c20b09331a02c8a2cf01a0e8a77bdc909cde
Romain Moro <moro.romain@gmail.com>
2018-07-03 18:31:13 +0200

    Add missing changes to previous commit.

--- src/GraphQL/Resolver/StyleResolver.php
+++ src/GraphQL/Resolver/StyleResolver.php
@@ -1,12 +1,34 @@
 <?php
-/**
- * Created by IntelliJ IDEA.
- * User: c4ilus
- * Date: 03/07/18
- * Time: 17:54
- */
 
-class StyleResolver
-{
+namespace App\GraphQL\Resolver;
 
+use Doctrine\ORM\EntityManager;
+use Overblog\GraphQLBundle\Definition\Argument;
+use Overblog\GraphQLBundle\Definition\Resolver\AliasedInterface;
+use Overblog\GraphQLBundle\Definition\Resolver\ResolverInterface;
+
+class StyleResolver implements ResolverInterface, AliasedInterface {
+
+  private $em;
+
+  public function __construct(EntityManager $em)
+  {
+    $this->em = $em;
+  }
+
+  public function resolve(Argument $args)
+  {
+    $style = $this->em->getRepository('App:Style')->find($args['id']);
+    return $style;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getAliases()
+  {
+    return [
+      'resolve' => 'Style'
+    ];
+  }
 }
