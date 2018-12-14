d1f488eda112199434b7ba089f99db2775d5a64e
Romain Moro <moro.romain@gmail.com>
2018-12-14 17:59:21 +0100

    Add the basic Music style mutations.

---
+++ volumes/code/src/GraphQL/Mutation/StyleMutation.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\GraphQL\Mutation;
+
+use App\Entity\Style;
+use App\Repository\StyleRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use GraphQL\Language\AST\ArgumentNode;
+use Overblog\GraphQLBundle\Definition\Resolver\AliasedInterface;
+use Overblog\GraphQLBundle\Definition\Resolver\MutationInterface;
+
+class StyleMutation implements MutationInterface, AliasedInterface
+{
+    private $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
+    /**
+     * @param array $argument
+     * @return array
+     */
+    public function addStyle(array $argument): array
+    {
+        $style = new Style();
+        $style->setName($argument['name']);
+
+        $this->em->persist($style);
+        $this->em->flush();
+
+        return [
+            'id' => $style->getId()
+        ];
+    }
+
+    /**
+     * Returns methods aliases.
+     *
+     * For instance:
+     * array('myMethod' => 'myAlias')
+     *
+     * @return array
+     */
+    public static function getAliases(): array
+    {
+        return [
+            'AddStyle' => 'add_style'
+        ];
+    }
+}
